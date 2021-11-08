c0ddf07617172e2dd5d5c6a7dcf90abc4841d7a6
newsight78 <newsight78@gmail.com>
2021-11-09 00:17:03 +0600

    람다식을 사용하였으며 메인 함수의 내용을 별도 함수로 빼냄. 차후 여러 코드 적용이 용이하기 위해

--- ProjectCpp/main.cpp
+++ ProjectCpp/main.cpp
@@ -1,87 +1,94 @@
-#include <stdio.h>
-#include <iostream>
-#include <vector>
+
 #include "main.h"
 
 using namespace std;
 
 int main(void)
 {
+    calculate();
+    return 0;
+}
+
+bool calculate()
+{
     long num1 = 0;
     long num2 = 0;
     bool isFinished = false;
     char calcOperator = 0;
-    
+
     try {
-        printf("첫 번째 숫자 : ");   scanf("%ld", &num1);    rewind(stdin);
-        printf("두 번째 숫자 : ");   scanf("%ld", &num2);    rewind(stdin);
-        
-        while(true)
+        printf("첫 번째 숫자 : ");
+        scanf("%ld", &num1);
+        rewind(stdin);
+        printf("두 번째 숫자 : ");
+        scanf("%ld", &num2);
+        rewind(stdin);
+
+        while (true)
         {
-            printf("연산을 입력해 주세요.(+, -, *, /) : ");  scanf("%c", &calcOperator); rewind(stdin);
-            
-            switch(calcOperator)
+            printf("연산을 입력해 주세요.(+, -, *, /) : ");
+            scanf("%c", &calcOperator);
+            rewind(stdin);
+
+            switch (calcOperator)
             {
-                case '+' :
-                    printf("%ld + %ld = %ld\n", num1, num2, num1 + num2);
-                    isFinished=true;
+            case '+':
+                printf("%ld + %ld = %ld\n", num1, num2, num1 + num2);
+                isFinished = true;
+                break;
+            case '-':
+                printf("%ld - %ld = %ld\n", num1, num2, num1 - num2);
+                isFinished = true;
+                break;
+            case '*':
+                printf("%ld * %ld = %ld\n", num1, num2, num1 * num2);
+                isFinished = true;
+                break;
+            case '/':
+                if (num2 != 0)
+                {
+                    printf("%ld / %ld = %.10lf\n", num1, num2, double(num1) / double(num2));
+                    isFinished = true;
                     break;
-                case '-' :
-                    printf("%ld - %ld = %ld\n", num1, num2, num1 - num2);
-                    isFinished=true;
-                    break;
-                case '*' :
-                    printf("%ld * %ld = %ld\n", num1, num2, num1 * num2);
-                    isFinished=true;
-                    break;
-                case '/' :
-                    if(num2 != 0) {
-                        printf("%ld / %ld = %.10lf\n", num1, num2, double(num1) / double(num2));
-                        isFinished=true;
-                        break;
-                    }
-                    else {
-                        printf("0으로 나눌 수 없습니다.\n");
-                        printf("두 번째 숫자 : ");   scanf("%ld", &num2);    rewind(stdin);
-                        continue;
-                    }
-                default:
+                }
+                else
+                {
+                    printf("0으로 나눌 수 없습니다.\n");
+                    printf("두 번째 숫자 : ");
+                    scanf("%ld", &num2);
+                    rewind(stdin);
                     continue;
+                }
+            default:
+                continue;
             }
-            
-            if(isFinished == true) {
+
+            if (isFinished == true)
+            {
                 compareNumbers(1, 1);
                 break;
             }
         }
     }
-    catch(const char* err) {
+    catch (const char *err)
+    {
         printf("%s\n", err);
     }
-    return 0;
+    return true;
 }
 
 bool compareNumbers(long num1st, long num2nd)
 {
-    vector<int> vt;
+    std::array<int, 5> numbers = {1, 2, 3, 4, 5};
+    int sum = 0;
 
-    auto i = "Outs";
+    std::for_each(numbers.begin(), numbers.end(), SumFunctor(sum));
 
-    auto y = [](int first, int second)
-    {
-        return first + second;
-    };
+    sum = 0;
 
-    vt.push_back(y(1, 2));
-    vt.push_back(y(2, 2));
-    vt.push_back(y(3, 2));
-    vt.push_back(y(4, 2));
+    // lambda로 구현
+    std::for_each(numbers.begin(), numbers.end(), [&sum](int &number)
+                  { sum += number; });
 
-    printf("%s", i);
-
-    if(num1st == num2nd)
-    {
-        return true;
-    }
-    return false;
+    return true;
 }
