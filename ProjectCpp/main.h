bcde1a5fcf90d35dc5a200511464e27e833e7963
newsight78 <newsight78@gmail.com>
2021-11-08 16:38:52 +0600

    Add files via upload

---
+++ ProjectCpp/main.h
@@ -0,0 +1,15 @@
+//
+//  main.h
+//  ProjectCpp
+//
+//  Created by KI HOON KIM on 08.11.2021.
+//
+
+#ifndef main_h
+#define main_h
+
+int main();
+
+bool compareNumbers(long num1st, long num2nd);
+
+#endif /* main_h */
